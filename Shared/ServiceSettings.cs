e9150e087197d90b344bbdb129192ee74d8fadf5
SmirnovAleksander <73950970+SmirnovAleksander@users.noreply.github.com>
2026-05-20 16:40:02 +0300

    add

--- Shared/ServiceSettings.cs
+++ Shared/ServiceSettings.cs
@@ -1,9 +1,27 @@
 namespace Shared;
 
-public record ServiceSettings(
-    string MainDbConnection,
-    Dictionary<string, string> RegionConnections,
-    string MqHost,
-    string Exchange,
-    string Queue,
-    string EventsExchange);
+public class ServiceSettings
+{
+    public string MainDbConnection { get; init; }
+    public Dictionary<string, string> RegionConnections { get; init; }
+    public string MqHost { get; init; }
+    public string Exchange { get; init; }
+    public string Queue { get; init; }
+    public string EventsExchange { get; init; }
+
+    public ServiceSettings(
+        string mainDbConnection,
+        Dictionary<string, string> regionConnections,
+        string mqHost,
+        string exchange,
+        string queue,
+        string eventsExchange)
+    {
+        MainDbConnection = mainDbConnection;
+        RegionConnections = regionConnections;
+        MqHost = mqHost;
+        Exchange = exchange;
+        Queue = queue;
+        EventsExchange = eventsExchange;
+    }
+}
